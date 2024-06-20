40e3a87f04813bc68382d1169febcaed81ceb958
Yashratho <91799322+Yashratho@users.noreply.github.com>
2024-06-20 15:58:09 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,49 +1,51 @@
-var score =0;
-var hitrn=0;
+var score = 0;
+var hitrn = 0;
 
-function increaseScore(){
-    score +=10;
-    document.querySelector ("#scoreval").textContent=score;
+function increaseScore() {
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
 }
-
-
 
 function getNewHit() {
     hitrn = Math.floor(Math.random() * 20);
     document.querySelector("#hit1").textContent = hitrn;
 }
 
-function makeBubble(){
-var clutter="";
-for (var i=1;i<=152;i++){
-    var rn=Math.floor(Math.random()*20)
-   clutter+= `<div class="bubble"><h3>${rn}</h3></div>`;
-}
-document.querySelector("#pbtm").innerHTML=clutter;}
-var timer =60;
-function runTimer(){
-  var timerint=  setInterval(function(){
-        if(timer>0){
-         timer--;
-        document.querySelector("#timerval").textContent=timer;}
-        else{
-            clearInterval(timerint);
-            document.querySelector("#pbtm").innerHTML=`<h1>GAME OVER</h1>`
-        }
-    },1000);
+function makeBubble() {
+    var clutter = "";
+    for (var i = 1; i <= 152; i++) {
+        var rn = Math.floor(Math.random() * 20);
+        clutter += `<div class="bubble"><h3>${rn}</h3></div>`;
+    }
+    document.querySelector("#pbtm").innerHTML = clutter;
 }
 
-document.querySelector("#pbtm").addEventListener("click",function(dets){
-var clickednum =Number(dets.target.textContent);
-if(clickednum===hitrn){
-    increaseScore();
-    makeBubble();
-    getNewHit();
-} 
+var timer = 60;
+function runTimer() {
+    var timerint = setInterval(function () {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        } else {
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML = `<h1>GAME OVER</h1>`;
+        }
+    }, 1000);
+}
+
+document.querySelector("#pbtm").addEventListener("click", function (dets) {
+    var clickednum = Number(dets.target.textContent);
+    if (clickednum === hitrn) {
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
 });
 
+// Start button logic
+document.querySelector("#startBtn").addEventListener("click", function () {
+   runTimer();
+    makeBubble();
+    getNewHit();
+});
 
-getNewHit()
-runTimer();
-makeBubble();
-setInterval();
